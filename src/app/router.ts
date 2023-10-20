5a75923c78998f1cfa54ff4cb8f56e78d721a9a5
merveakca <smkirac@gmail.com>
2023-10-20 22:12:17 +0300

    first commit

---
+++ src/app/router.ts
@@ -0,0 +1,21 @@
+import { Routes } from "@angular/router";
+import { LayoutsComponent } from "./components/layouts/layouts.component";
+import { BlankComponent } from "./components/blank/blank.component";
+import { LoginComponent } from "./components/login/login.component";
+
+export const router: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "",
+        component: LayoutsComponent,
+        children:[
+            {
+                path: "",
+                component: BlankComponent
+            }
+        ]
+    }
+]
